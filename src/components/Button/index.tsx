422a04af96a817d5ad816e90a08834bbf1f56929
Flávio Nagle <flavio.nagle@monitoratec.com.br>
2022-01-07 17:09:44 -0300

    commit inicial

--- src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import {Button as MuiButton, ButtonProps} from '@mui/material';
-
-const Button = (props: ButtonProps) => {
-  return <MuiButton {...props}/>
-}
-
-export default Button;
--- src/components/TextField/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import {TextField as MuiTextField, TextFieldProps} from '@mui/material';
-
-const TextField = (props: TextFieldProps) => {
-  return <MuiTextField {...props}/>
-}
-
-export default TextField;
